3482d8187d84c5734862a164551c8eaaf5940252
Daniel Urban <urban.dani@gmail.com>
2021-01-05 16:21:10 +0100

    Decrease iterations in StmStackSpec (CI timeout)

--- bench/src/test/scala/dev/tauri/choam/bench/util/StmStackSpec.scala
+++ bench/src/test/scala/dev/tauri/choam/bench/util/StmStackSpec.scala
@@ -50,7 +50,7 @@ class StmStackSpec extends CatsEffectSuite with BaseSpecA {
 
   test("StmStack should not lose items") {
     val s = new StmStack[Int]
-    val N = 1000000
+    val N = 100000
     val seed1 = ThreadLocalRandom.current().nextInt()
     val seed2 = ThreadLocalRandom.current().nextInt()
     def push(xs: XorShift): Unit = {
@@ -101,7 +101,7 @@ class StmStackSpec extends CatsEffectSuite with BaseSpecA {
   test("StmStack should have composable transactions") {
     val s1 = new StmStack[Int]
     val s2 = new StmStack[Int]
-    val N = 1000000
+    val N = 100000
     def push(xs: XorShift): Unit = {
       for (_ <- 1 to N) {
         val item = xs.nextInt()
