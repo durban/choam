562eb0421c6baf76feec7ae061a17641f7faeb25
Daniel Urban <urban.dani@gmail.com>
2021-01-19 00:07:08 +0100

    Add composed promise stress test

---
+++ stress/src/test/scala/dev/tauri/choam/async/PromiseComplete2Test.scala
@@ -0,0 +1,66 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2016-2021 Daniel Urban and contributors listed in NOTICE.txt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.tauri.choam
+package async
+
+import org.openjdk.jcstress.annotations._
+import org.openjdk.jcstress.annotations.Outcome.Outcomes
+import org.openjdk.jcstress.annotations.Expect._
+import org.openjdk.jcstress.infra.results.ZLL_Result
+
+import cats.effect.{ IO, SyncIO }
+
+@JCStressTest
+@State
+@Description("Promise: completing 2 promises should occur atomically")
+@Outcomes(Array(
+  new Outcome(id = Array("true, (None,None), (None,None)"), expect = ACCEPTABLE, desc = ""),
+  new Outcome(id = Array("true, (None,None), (Some(x),Some(y))"), expect = ACCEPTABLE, desc = ""),
+  new Outcome(id = Array("true, (Some(x),Some(y)), (None,None)"), expect = ACCEPTABLE, desc = ""),
+  new Outcome(id = Array("true, (Some(x),Some(y)), (Some(x),Some(y))"), expect = ACCEPTABLE, desc = "")
+))
+class PromiseComplete2Test {
+
+  val p1: Promise[IO, String] =
+    Promise[IO, String].run[SyncIO].unsafeRunSync()
+
+  val p2: Promise[IO, String] =
+    Promise[IO, String].run[SyncIO].unsafeRunSync()
+
+  val completeBoth: React[(String, String), (Boolean, Boolean)] =
+    p1.complete × p2.complete
+
+  val tryGetBoth: React[Unit, (Option[String], Option[String])] =
+    p1.tryGet * p2.tryGet
+
+  @Actor
+  def complete(r: ZLL_Result): Unit = {
+    val res = completeBoth[SyncIO](("x", "y")).unsafeRunSync()
+    r.r1 = res._1 && res._2
+  }
+
+  @Actor
+  def get1(r: ZLL_Result): Unit = {
+    r.r2 = tryGetBoth.run[SyncIO].unsafeRunSync()
+  }
+
+  @Actor
+  def get2(r: ZLL_Result): Unit = {
+    r.r3 = tryGetBoth.run[SyncIO].unsafeRunSync()
+  }
+}
