8936a9068d8514e384153d08b4557158fa319d1b
Daniel Urban <urban.dani@gmail.com>
2021-03-10 20:35:54 +0100

    Failing test for non-composable MS-queue deq

---
+++ stress/src/test/scala/dev/tauri/choam/MichaelScottQueueComposedTest3.scala
@@ -0,0 +1,62 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2016-2021 Daniel Urban and contributors listed in NOTICE.txt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.tauri.choam
+
+import org.openjdk.jcstress.annotations.{ Ref => _, _ }
+import org.openjdk.jcstress.annotations.Outcome.Outcomes
+import org.openjdk.jcstress.annotations.Expect._
+import org.openjdk.jcstress.infra.results.LL_Result
+
+import kcas.Ref
+
+@JCStressTest
+@State
+@Description("Michael-Scott queue tricky enq/deq")
+@Outcomes(Array(
+  new Outcome(id = Array("(None,false), true"), expect = ACCEPTABLE, desc = "deq was first"),
+  new Outcome(id = Array("(Some(a),true), false"), expect = ACCEPTABLE, desc = "enq was first"),
+  // TODO: this is not acceptable, but a bug:
+  new Outcome(id = Array("(None,true), false"), expect = ACCEPTABLE_INTERESTING, desc = "enq was first, but deq sees empty")
+))
+class MichaelScottQueueComposedTest3 extends StressTestBase {
+
+  private[this] val queue =
+    new MichaelScottQueue[String]()
+
+  private[this] val latch =
+    Ref.mk[Boolean](false)
+
+  private[this] val dummy =
+    Ref.mk[Int](0)
+
+  private[this] val deq =
+    queue.tryDeque * (dummy.modify { _ + 1 }.flatMap { _ => latch.modify(_ => true) })
+
+  private[this] val enq =
+    (queue.enqueue * latch.modify(_ => true)).map(_._2)
+
+  @Actor
+  def deq(r: LL_Result): Unit = {
+    r.r1 = deq.unsafePerform((), this.impl)
+  }
+
+  @Actor
+  def enq(r: LL_Result): Unit = {
+    r.r2 = enq.unsafePerform("a", this.impl)
+  }
+}
