1e23f0d93af738470f6763752155574a559164cb
Daniel Urban <urban.dani@gmail.com>
2022-03-25 16:03:57 +0100

    CI: Try to fix timeout

--- async/shared/src/test/scala/dev/tauri/choam/async/OverflowQueueSpec.scala
+++ async/shared/src/test/scala/dev/tauri/choam/async/OverflowQueueSpec.scala
@@ -61,7 +61,7 @@ trait OverflowQueueSpec[F[_]]
     OverflowQueue.droppingQueue(capacity).run[F]
 
   final val Max =
-    3072
+    2048
 
   test("RingBuffer property") {
     def checkSize[A](q: OverflowQueue[F, A], qc: CatsQueue[F, A], s: CatsQueue[F, A]): F[Unit] = {
