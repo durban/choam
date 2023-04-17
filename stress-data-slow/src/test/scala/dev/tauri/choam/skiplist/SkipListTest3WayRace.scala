7b6616495e3e3f9bdb95491c217dc769a7e1f0cb
Daniel Urban <urban.dani@gmail.com>
2023-04-17 23:58:40 +0200

    SkipListMap: update the last jcstress test

--- stress-data-slow/src/test/scala/dev/tauri/choam/skiplist/SkipListTest3WayRace.scala
+++ stress-data-slow/src/test/scala/dev/tauri/choam/skiplist/SkipListTest3WayRace.scala
@@ -21,80 +21,49 @@ package skiplist
 import org.openjdk.jcstress.annotations.{ Ref => _, Outcome => JOutcome, _ }
 import org.openjdk.jcstress.annotations.Expect._
 import org.openjdk.jcstress.annotations.Outcome.Outcomes
-import org.openjdk.jcstress.infra.results.JJ_Result
+import org.openjdk.jcstress.infra.results.LLLL_Result
 
 @JCStressTest
 @State
-@Description("TimerSkipList: 3-way insert/pollFirstIfTriggered/cancel race")
+@Description("SkipListMap: 3-way get/put/del race")
 @Outcomes(Array(
-  new JOutcome(id = Array("3, 2"), expect = ACCEPTABLE_INTERESTING, desc = "(cancel | insert), pollFirst"),
-  new JOutcome(id = Array("1, 3"), expect = ACCEPTABLE_INTERESTING, desc = "pollFirst, (cancel | insert) OR insert, pollFirst, cancel"),
-  new JOutcome(id = Array("2, 3"), expect = ACCEPTABLE_INTERESTING, desc = "cancel, pollFirst, insert"),
+  new JOutcome(id = Array("Some(64), Some(64), true, None"), expect = ACCEPTABLE_INTERESTING, desc = "get, put, del"),
+  new JOutcome(id = Array("Some(64), None, true, Some(VAL)"), expect = ACCEPTABLE_INTERESTING, desc = "get, del, put"),
+  new JOutcome(id = Array("Some(VAL), Some(64), true, None"), expect = ACCEPTABLE_INTERESTING, desc = "put, get, del"),
+  new JOutcome(id = Array("None, Some(64), true, None"), expect = ACCEPTABLE_INTERESTING, desc = "put, del, get"),
+  new JOutcome(id = Array("None, None, true, Some(VAL)"), expect = ACCEPTABLE_INTERESTING, desc = "del, get, put"),
+  new JOutcome(id = Array("Some(VAL), None, true, Some(VAL"), expect = ACCEPTABLE_INTERESTING, desc = "del, put, get"),
 ))
 class SkipListTest3WayRace {
 
-  import SkipListHelper.Callback
-
-  /*
-   * Concurrently (1) cancel the first item,
-   * (2) insert a new one right after it, and
-   * (3) deque the first item.
-   */
-
-  private[this] val secondCb =
-    newCallback()
+  private[this] final val KEY =
+    64L
 
   private[this] val m = {
-    val m = new SkipListMap[Long, Callback]
-    m.insertTlr(2L + 1024L, secondCb)
-    for (i <- 3 to 128) {
-      m.insertTlr(i.toLong + 1024L, newCallback())
+    val m = new SkipListMap[Long, String]
+    for (i <- 1L to 128L) {
+      m.put(i, i.toString)
     }
     m
   }
 
-  private[this] val headCb =
-    newCallback()
-
-  private[this] val headCanceller =
-    m.insertTlr(1L + 1024L, headCb)
-
-  private[this] val newCb =
-    newCallback()
-
   @Actor
-  def cancel(): Unit = {
-    headCanceller.run()
+  def get(r: LLLL_Result): Unit = {
+    r.r1 = m.get(KEY)
   }
 
   @Actor
-  def insert(): Unit = {
-    // head is 1025L now, we insert another 1025L:
-    m.insertTlr(1L + 1024L, newCb)
-    ()
+  def put(r: LLLL_Result): Unit = {
+    r.r2 = m.put(KEY, "VAL")
   }
 
   @Actor
-  def pollFirst(r: JJ_Result): Unit = {
-    r.r1 = longFromCb(m.pollFirstIfTriggered(2048L))
+  def del(r: LLLL_Result): Unit = {
+    r.r3 = m.del(KEY)
   }
 
   @Arbiter
-  def arbiter(r: JJ_Result): Unit = {
-    r.r2 = longFromCb(m.pollFirstIfTriggered(2048L))
-  }
-
-  private[this] final def longFromCb(cb: Callback): Long = {
-    if (cb eq headCb) 1L
-    else if (cb eq secondCb) 2L
-    else if (cb eq newCb) 3L
-    else -1L
-  }
-
-  private[this] final def newCallback(): Callback = {
-    new Function1[Right[Nothing, Unit], Unit] with Serializable {
-      final override def apply(r: Right[Nothing, Unit]): Unit = ()
-      final override def toString: String = s"Callback(#${this.##.toHexString})"
-    }
+  def arbiter(r: LLLL_Result): Unit = {
+    r.r4 = m.get(KEY)
   }
 }
