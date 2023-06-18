3225e5a78a14597f05531a0f79969e191c5e111c
Daniel Urban <urban.dani@gmail.com>
2023-06-18 19:19:14 +0200

    More tests

--- core/shared/src/test/scala/dev/tauri/choam/OsRngSpec.scala
+++ core/shared/src/test/scala/dev/tauri/choam/OsRngSpec.scala
@@ -17,20 +17,64 @@
 
 package dev.tauri.choam
 
-import cats.syntax.all._
+import java.util.Arrays
+
+import cats.effect.IO
+import cats.effect.instances.spawn.parallelForGenSpawn
 
 import random.OsRng
 
-final class OsRngSpec extends BaseSpec {
+final class OsRngSpecIO
+  extends BaseSpecIO
+  with OsRngSpec[IO]
+  with SpecNoMcas
+
+trait OsRngSpec[F[_]] extends BaseSpecAsyncF[F] { this: McasImplSpec =>
 
   test("OsRng#nextBytes") {
     val rng = OsRng.mkNew()
     assert(Either.catchOnly[IllegalArgumentException] { rng.nextBytes(-1) }.isLeft)
-    assertEquals(rng.nextBytes(0).length, 0)
-    assertEquals(rng.nextBytes(1).length, 1)
-    assertEquals(rng.nextBytes(256).length, 256)
-    assertEquals(rng.nextBytes(257).length, 257)
-    assertEquals(rng.nextBytes(4096).length, 4096)
+    assert(Either.catchOnly[IllegalArgumentException] { rng.nextBytes(-1000) }.isLeft)
+    assert(Either.catchOnly[IllegalArgumentException] { rng.nextBytes(Int.MinValue) }.isLeft)
+    checkNextBytes(rng, size = 0)
+    checkNextBytes(rng, size = 1)
+    checkNextBytes(rng, size = 256)
+    checkNextBytes(rng, size = 257)
+    checkNextBytes(rng, size = 4096)
+    checkNextBytes(rng, size = 32768)
     assert(rng.nextBytes(4).exists(_ != 0.toByte))
+  }
+
+  private def checkNextBytes(rng: OsRng, size: Int): Unit = {
+    val a = rng.nextBytes(size)
+    assertEquals(a.length, size)
+    val b = rng.nextBytes(size)
+    assertEquals(b.length, size)
+    if (size > 2) {
+      // at most 1/16777216 chance of accidental failure
+      assert(!Arrays.equals(a, b))
+    }
+  }
+
+  final val N = 256
+
+  val full0 = new Array[Byte](N)
+
+  test("Multi-threaded use") {
+    val rng = OsRng.mkNew()
+    useInParallel(List.fill(4)(rng))
+  }
+
+  test("Use different RNGs") {
+    useInParallel(List.fill(4) { OsRng.mkNew() })
+  }
+
+  private def useInParallel(rngs: List[OsRng]): F[Unit] = {
+    rngs.parTraverse { rng =>
+      F.delay(new Array[Byte](N)).flatMap { buff =>
+        val once = F.delay(rng.nextBytes(buff)) >> F.delay(assert(!Arrays.equals(buff, full0))) >> F.delay(Arrays.fill(buff, 0.toByte))
+        (once >> F.cede).replicateA_(4096)
+      }
+    }.void
   }
 }
