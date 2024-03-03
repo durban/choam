5a309c9add640336aca5b9d857560cc2852dad24
Daniel Urban <urban.dani@gmail.com>
2024-03-03 05:55:26 +0100

    Updated RefArrayLazyRaceTest [stressCore]

--- stress/stress-core/src/test/scala/dev/tauri/choam/RefArrayLazyRaceTest.scala
+++ stress/stress-core/src/test/scala/dev/tauri/choam/RefArrayLazyRaceTest.scala
@@ -20,43 +20,44 @@ package dev.tauri.choam
 import org.openjdk.jcstress.annotations.{ Ref => _, _ }
 import org.openjdk.jcstress.annotations.Outcome.Outcomes
 import org.openjdk.jcstress.annotations.Expect._
-import org.openjdk.jcstress.infra.results.LLLL_Result
+import org.openjdk.jcstress.infra.results.LLLLL_Result
 
-// @JCStressTest
+@JCStressTest
 @State
-@Description("LazyRefArray Refs should be safely created")
+@Description("SparseRefArray Refs should be safely created")
 @Outcomes(Array(
-  new Outcome(id = Array("ok, ok, x, x"), expect = ACCEPTABLE, desc = "correct lazy init"),
+  new Outcome(id = Array("ok, ok, x, y1, y2"), expect = ACCEPTABLE, desc = "upd1 won"),
+  new Outcome(id = Array("ok, ok, y2, x, y1"), expect = ACCEPTABLE, desc = "upd2 won"),
 ))
 class RefArrayLazyRaceTest extends StressTestBase {
 
-  private[this] final val arr: Ref.Array[String] = {
-    val a = Ref.unsafeArray[String](4, "x", Ref.Array.AllocationStrategy.SparseFlat)
-    a.unsafeGet(0).loc.unsafeSetVolatile("-")
-    a.unsafeGet(1).loc.unsafeSetVolatile("-")
-    // we don't change/initialize `a(2)`
-    a.unsafeGet(3).loc.unsafeSetVolatile("-")
-    a
-  }
+  private[this] val f1: Function1[String, String] =
+    { _ => "y1" }
+
+  private[this] val f2: Function1[String, String] =
+    { _ => "y2" }
+
+  private[this] final val arr: Ref.Array[String] =
+    Ref.unsafeArray[String](4, "x", Ref.Array.AllocationStrategy.SparseFlat)
 
   @Actor
-  def one(r: LLLL_Result): Unit = {
+  def upd1(r: LLLLL_Result): Unit = {
     val ref = this.arr.unsafeGet(2)
-    val value = ref.loc.unsafeGetVolatile()
+    val value = ref.getAndUpdate(f1).unsafeRun(this.impl)
     r.r1 = ref
     r.r3 = value
   }
 
   @Actor
-  def two(r: LLLL_Result): Unit = {
+  def upd2(r: LLLLL_Result): Unit = {
     val ref = this.arr.unsafeGet(2)
-    val value = ref.loc.unsafeGetVolatile()
+    val value = ref.getAndUpdate(f2).unsafeRun(this.impl)
     r.r2 = ref
     r.r4 = value
   }
 
   @Arbiter
-  def arbiter(r: LLLL_Result): Unit = {
+  def arbiter(r: LLLLL_Result): Unit = {
     if (r.r1 eq r.r2) {
       // OK, the 2 refs are the same
       r.r1 = "ok"
@@ -66,5 +67,6 @@ class RefArrayLazyRaceTest extends StressTestBase {
       r.r1 = r.r1.toString()
       r.r2 = r.r2.toString()
     }
+    r.r5 = this.arr.unsafeGet(2).get.unsafeRun(this.impl)
   }
 }
