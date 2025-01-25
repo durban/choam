fd7359b93242d5dddd9a61a372609446f80ea082
Daniel Urban <urban.dani@gmail.com>
2025-01-25 14:40:28 +0100

    Move tests to their proper place

--- stm/shared/src/test/scala/dev/tauri/choam/stm/TRefSpec.scala
+++ stm/shared/src/test/scala/dev/tauri/choam/stm/TRefSpec.scala
@@ -18,7 +18,11 @@
 package dev.tauri.choam
 package stm
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import cats.effect.IO
+
+import internal.mcas.{ Consts, MemoryLocation }
 
 final class TRefSpec_DefaultMcas_IO
   extends BaseSpecIO
@@ -35,11 +39,76 @@ trait TRefSpec[F[_]] extends TxnBaseSpec[F] { this: McasImplSpec =>
   protected def newTRef[A](initial: A): F[TRef[F, A]] =
     TRef[F, A](initial).commit
 
+  test("TRef#update") {
+    for {
+      r <- newTRef(0)
+      _ <- assertResultF(r.update(_ + 1).commit, ())
+      _ <- assertResultF(r.get.commit, 1)
+    } yield ()
+  }
+
+  test("TRef#modify") {
+    for {
+      r <- newTRef(0)
+      _ <- assertResultF(r.modify(ov => (ov + 1, 42)).commit, 42)
+      _ <- assertResultF(r.get.commit, 1)
+    } yield ()
+  }
+
+  test("TRef#getAndSet") {
+    for {
+      r <- newTRef(0)
+      _ <- assertResultF(r.getAndSet(42).commit, 0)
+      _ <- assertResultF(r.get.commit, 42)
+    } yield ()
+  }
+
   test("TRef#updateAndGet") {
     for {
       ref <- newTRef(42)
       _ <- assertResultF(ref.updateAndGet(_ + 1).commit, 43)
       _ <- assertResultF(ref.get.commit, 43)
+    } yield ()
+  }
+
+  test("TRef should have .withListeners") {
+    def incr(ref: TRef[F, Int]): F[Unit] =
+      ref.get.flatMap { ov => ref.set(ov + 1) }.commit
+    def getVersion(loc: MemoryLocation[Int]): F[Long] =
+      Txn.unsafe.delayContext { ctx => ctx.readIntoHwd(loc).version }.commit
+    def regListener(wl: MemoryLocation.WithListeners, cb: Null => Unit, lastSeenVersion: Long): F[Long] =
+      Txn.unsafe.delayContext { ctx => wl.unsafeRegisterListener(ctx, cb, lastSeenVersion) }.commit
+    def check(ref: TRef[F, Int]): F[Unit] = for {
+      loc <- F.delay(ref.asInstanceOf[MemoryLocation[Int]])
+      wl <- F.delay(loc.withListeners)
+      ctr <- F.delay(new AtomicInteger(0))
+      firstVersion <- getVersion(loc)
+      // registered listener should be called:
+      lid <- regListener(wl, { _ => ctr.getAndIncrement(); () }, firstVersion)
+      _ <- assertNotEqualsF(lid, Consts.InvalidListenerId)
+      _ <- incr(ref)
+      _ <- F.delay(assertEquals(ctr.get(), 1))
+      // after it was called once, it shouldn't any more:
+      _ <- incr(ref)
+      _ <- F.delay(assertEquals(ctr.get(), 1))
+      // registered, but then cancelled listener shouldn't be called:
+      otherVersion <- getVersion(loc)
+      _ <- assertF(firstVersion < otherVersion)
+      lid <- regListener(wl, { _ => ctr.getAndIncrement(); () }, otherVersion)
+      _ <- assertNotEqualsF(lid, Consts.InvalidListenerId)
+      _ <- F.delay(wl.unsafeCancelListener(lid))
+      _ <- incr(ref)
+      _ <- F.delay(assertEquals(ctr.get(), 1))
+      // failed registration due to outdated `lastSeenVersion`:
+      lid <- regListener(wl, { _ => ctr.getAndIncrement(); () }, firstVersion)
+      _ <- assertEqualsF(lid, Consts.InvalidListenerId)
+      _ <- incr(ref)
+      _ <- F.delay(assertEquals(ctr.get(), 1))
+    } yield ()
+
+    for {
+      r <- newTRef(42)
+      _ <- check(r)
     } yield ()
   }
 }
--- stm/shared/src/test/scala/dev/tauri/choam/stm/TxnSpec.scala
+++ stm/shared/src/test/scala/dev/tauri/choam/stm/TxnSpec.scala
@@ -18,7 +18,7 @@
 package dev.tauri.choam
 package stm
 
-import java.util.concurrent.atomic.{ AtomicBoolean, AtomicInteger }
+import java.util.concurrent.atomic.AtomicBoolean
 
 import scala.concurrent.duration._
 
@@ -27,9 +27,6 @@ import cats.{ Defer, Monad, StackSafeMonad }
 import cats.effect.kernel.Unique
 import cats.effect.{ IO, Deferred }
 import cats.effect.Outcome.{ Canceled, Succeeded, Errored }
-
-import internal.mcas.MemoryLocation
-import internal.mcas.Consts
 
 final class TxnSpec_DefaultMcas_IO
   extends BaseSpecIO
@@ -83,47 +80,6 @@ trait TxnSpec[F[_]] extends TxnBaseSpec[F] { this: McasImplSpec =>
       _ <- r.set(1).commit
       _ <- assertResultF(d.get, "1")
       _ <- fib.joinWithNever
-    } yield ()
-  }
-
-  test("TRef should have .withListeners") {
-    def incr(ref: TRef[F, Int]): F[Unit] =
-      ref.get.flatMap { ov => ref.set(ov + 1) }.commit
-    def getVersion(loc: MemoryLocation[Int]): F[Long] =
-      Txn.unsafe.delayContext { ctx => ctx.readIntoHwd(loc).version }.commit
-    def regListener(wl: MemoryLocation.WithListeners, cb: Null => Unit, lastSeenVersion: Long): F[Long] =
-      Txn.unsafe.delayContext { ctx => wl.unsafeRegisterListener(ctx, cb, lastSeenVersion) }.commit
-    def check(ref: TRef[F, Int]): F[Unit] = for {
-      loc <- F.delay(ref.asInstanceOf[MemoryLocation[Int]])
-      wl <- F.delay(loc.withListeners)
-      ctr <- F.delay(new AtomicInteger(0))
-      firstVersion <- getVersion(loc)
-      // registered listener should be called:
-      lid <- regListener(wl, { _ => ctr.getAndIncrement(); () }, firstVersion)
-      _ <- assertNotEqualsF(lid, Consts.InvalidListenerId)
-      _ <- incr(ref)
-      _ <- F.delay(assertEquals(ctr.get(), 1))
-      // after it was called once, it shouldn't any more:
-      _ <- incr(ref)
-      _ <- F.delay(assertEquals(ctr.get(), 1))
-      // registered, but then cancelled listener shouldn't be called:
-      otherVersion <- getVersion(loc)
-      _ <- assertF(firstVersion < otherVersion)
-      lid <- regListener(wl, { _ => ctr.getAndIncrement(); () }, otherVersion)
-      _ <- assertNotEqualsF(lid, Consts.InvalidListenerId)
-      _ <- F.delay(wl.unsafeCancelListener(lid))
-      _ <- incr(ref)
-      _ <- F.delay(assertEquals(ctr.get(), 1))
-      // failed registration due to outdated `lastSeenVersion`:
-      lid <- regListener(wl, { _ => ctr.getAndIncrement(); () }, firstVersion)
-      _ <- assertEqualsF(lid, Consts.InvalidListenerId)
-      _ <- incr(ref)
-      _ <- F.delay(assertEquals(ctr.get(), 1))
-    } yield ()
-
-    for {
-      r <- TRef[F, Int](42).commit
-      _ <- check(r)
     } yield ()
   }
 
@@ -273,30 +229,6 @@ trait TxnSpec[F[_]] extends TxnBaseSpec[F] { this: McasImplSpec =>
       t12 <- generic[Txn[F, *]].commit
       (t1, t2) = t12
       _ <- assertNotEqualsF(t1, t2)
-    } yield ()
-  }
-
-  test("TRef#update") {
-    for {
-      r <- TRef[F, Int](0).commit
-      _ <- assertResultF(r.update(_ + 1).commit, ())
-      _ <- assertResultF(r.get.commit, 1)
-    } yield ()
-  }
-
-  test("TRef#modify") {
-    for {
-      r <- TRef[F, Int](0).commit
-      _ <- assertResultF(r.modify(ov => (ov + 1, 42)).commit, 42)
-      _ <- assertResultF(r.get.commit, 1)
-    } yield ()
-  }
-
-  test("TRef#getAndSet") {
-    for {
-      r <- TRef[F, Int](0).commit
-      _ <- assertResultF(r.getAndSet(42).commit, 0)
-      _ <- assertResultF(r.get.commit, 42)
     } yield ()
   }
 }
