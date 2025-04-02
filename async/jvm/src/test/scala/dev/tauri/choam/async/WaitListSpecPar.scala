41517241ffed0c01d134cec107f2923c76821387
Daniel Urban <urban.dani@gmail.com>
2025-04-02 03:02:03 +0200

    Ignore expected failures

--- async/jvm/src/test/scala/dev/tauri/choam/async/WaitListSpecPar.scala
+++ async/jvm/src/test/scala/dev/tauri/choam/async/WaitListSpecPar.scala
@@ -114,9 +114,7 @@ trait WaitListSpecPar[F[_]] extends BaseSpecAsyncF[F] with AsyncReactiveSpec[F] 
       }
     }
 
-    // TODO: in theory these can fail, but in practice, they doesn't seem to...
-
-    test(s"$name: enqueue cancel race (1 waiter; bound = $bound)") {
+    test(s"$name: enqueue cancel race (1 waiter; bound = $bound)".ignore) { // TODO: should be .fail, but it's sporadic
       val t = for {
         _ <- assertF(bound > 0)
         q <- newEmptyQ
@@ -139,7 +137,7 @@ trait WaitListSpecPar[F[_]] extends BaseSpecAsyncF[F] with AsyncReactiveSpec[F] 
       t.parReplicateA_(50000)
     }
 
-    test(s"$name: enqueue cancel race (2 waiters; bound = $bound)") {
+    test(s"$name: enqueue cancel race (2 waiters; bound = $bound)".ignore) { // TODO: should be .fail, but it's sporadic
       val t = for {
         _ <- assertF(bound > 0)
         q <- newEmptyQ
