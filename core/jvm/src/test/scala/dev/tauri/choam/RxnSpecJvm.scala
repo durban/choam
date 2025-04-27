8c47dd9c5f4d6230edb5eb64f5431c2e3b9f0dec
Daniel Urban <urban.dani@gmail.com>
2025-04-28 00:07:09 +0200

    Try to fix a flaky test

--- core/jvm/src/test/scala/dev/tauri/choam/RxnSpecJvm.scala
+++ core/jvm/src/test/scala/dev/tauri/choam/RxnSpecJvm.scala
@@ -253,7 +253,7 @@ trait RxnSpecJvm[F[_]] extends RxnSpec[F] { this: McasImplSpec =>
   }
 
   test("unsafe.unread should make a conflict disappear") {
-    val N = 30000
+    val N = 40000
     def withoutUnread(r1: Ref[String], r2: Ref[String]): Axn[Int] = {
       // without unread, this will sometimes retry if
       // there is a concurrent change to `r1`, and will
