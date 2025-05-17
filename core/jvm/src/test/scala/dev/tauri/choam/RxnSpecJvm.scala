ff69192d2bd08745ebbde1b353184174a25c5603
Daniel Urban <urban.dani@gmail.com>
2025-05-17 02:11:35 +0200

    Additional test

--- core/jvm/src/test/scala/dev/tauri/choam/RxnSpecJvm.scala
+++ core/jvm/src/test/scala/dev/tauri/choam/RxnSpecJvm.scala
@@ -411,6 +411,30 @@ trait RxnSpecJvm[F[_]] extends RxnSpec[F] { this: McasImplSpec =>
     } yield ()
   }
 
+  test("unsafe.tentativeRead (can commit while depending on inconsistent state, but) still detects inconsistent WRITES") {
+    for {
+      r1 <- Ref(0).run[F]
+      r2 <- Ref(0).run[F]
+      ctr <- F.delay(new AtomicInteger(0))
+      latch1 <- F.delay(new CountDownLatch(1))
+      latch2 <- F.delay(new CountDownLatch(1))
+      rxn1 = Rxn.unsafe.tentativeRead(r1).flatMapF { ticket1 =>
+        Rxn.unsafe.tentativeRead(r2).flatMapF { ticket2 =>
+          ctr.incrementAndGet()
+          latch1.countDown()
+          // concurrent change to r2
+          latch2.await()
+          ticket2.unsafeSet(99).as((ticket1.unsafePeek, ticket2.unsafePeek))
+        }
+      }
+      rxn2Task = F.delay(latch1.await()) *> r2.update(_ + 1).run[F] *> F.delay(latch2.countDown())
+      _ <- assertResultF(F.both(rxn1.run[F], rxn2Task).map(_._1), (0, 1))
+      _ <- assertResultF(r1.get.run[F], 0)
+      _ <- assertResultF(r2.get.run[F], 99)
+      _ <- assertResultF(F.delay(ctr.get()), 2)
+    } yield ()
+  }
+
   test("Opacity with simple .get instead of unsafe.tentativeRead") {
     for {
       r1 <- Ref(0).run[F]
