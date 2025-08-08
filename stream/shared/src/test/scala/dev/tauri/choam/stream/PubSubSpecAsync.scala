9a01c529754405cc3257d180202625441b9a1414
Daniel Urban <urban.dani@gmail.com>
2025-08-08 02:20:04 +0200

    Also run parallel tests for PubSub.async

---
+++ stream/shared/src/test/scala/dev/tauri/choam/stream/PubSubSpecAsync.scala
@@ -0,0 +1,39 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2016-2025 Daniel Urban and contributors listed in NOTICE.txt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.tauri.choam
+package stream
+
+import cats.effect.IO
+
+final class PubSubSpecAsync_DefaultMcas_IO
+  extends BaseSpecIO
+  with SpecDefaultMcas
+  with PubSubSpecAsync[IO]
+
+final class PubSubSpecAsync_DefaultMcas_ZIO
+  extends BaseSpecZIO
+  with SpecDefaultMcas
+  with PubSubSpecAsync[zio.Task]
+
+trait PubSubSpecAsync[F[_]] extends PubSubSpec[F] { this: McasImplSpec =>
+
+  protected[this] final override type H[A] = PubSub[A]
+
+  protected[this] final override def newHub[A](str: PubSub.OverflowStrategy): F[PubSub[A]] =
+    PubSub.async[A](str).run[F]
+}
