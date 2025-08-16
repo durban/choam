f5ae31dc60f80937ce3e656dd37a2fb9af55f79a
Daniel Urban <urban.dani@gmail.com>
2025-08-16 12:06:47 +0200

    Fix SN OsRng on windows

--- mcas/native/src/main/scala/dev/tauri/choam/internal/mcas/OsRngPlatform.scala
+++ mcas/native/src/main/scala/dev/tauri/choam/internal/mcas/OsRngPlatform.scala
@@ -21,7 +21,7 @@ package mcas
 
 import scala.scalanative.meta.LinktimeInfo
 import scala.scalanative.unsigned.{ UnsignedRichInt, UInt }
-import scala.scalanative.unsafe.{ extern, link, CSize, CVoidPtr, CUnsignedLong, CInt, Ptr, UnsafeRichArray }
+import scala.scalanative.unsafe.{ extern, link, CSize, CVoidPtr, CInt, Ptr, UnsafeRichArray }
 import scala.scalanative.posix.unistd
 import scala.scalanative.libc.errno
 
@@ -43,43 +43,38 @@ private final class NativeOsRng extends OsRng {
 
   final override def nextBytes(dest: Array[Byte]): Unit = {
     val ptr = dest.at(0)
-    nextBytes(ptr, dest.length.toCSize)
+    if (LinktimeInfo.isWindows) {
+      nextBytesWin(ptr, dest.length.toUInt)
+    } else {
+      nextBytesUnix(ptr, dest.length.toCSize)
+    }
   }
 
   @tailrec
-  private[this] final def nextBytes(start: Ptr[Byte], remaining: CSize): Unit = {
+  private[this] final def nextBytesUnix(start: Ptr[Byte], remaining: CSize): Unit = {
     val max = 256.toUSize
     val size = if (remaining > max) max else remaining
     getentropy(start, size)
     val rem = remaining - size
     if (rem > 0.toUSize) {
-      nextBytes(start + size, rem)
+      nextBytesUnix(start + size, rem)
     } // else: we're done
   }
 
-  @inline
   private[this] final def getentropy(buffer: CVoidPtr, length: CSize): Unit = {
-    if (LinktimeInfo.isWindows) {
-      win(buffer, length)
-    } else {
-      unix(buffer, length)
-    }
-  }
-
-  private[this] final def unix(buffer: CVoidPtr, length: CSize): Unit = {
-    val res = unistd.getentropy(buffer, length)
+    val res: CInt = unistd.getentropy(buffer, length)
     if (res != 0) {
       val no: CInt = errno.errno
       throw new RuntimeException(s"getentropy returned ${res} (errno == ${no})")
     }
   }
 
-  private[this] final def win(buffer: CVoidPtr, length: CSize): Unit = {
+  private[this] final def nextBytesWin(buffer: CVoidPtr, length: UInt): Unit = {
     val status: UInt = Bcrypt.BCryptGenRandom(
       null,
       buffer,
       length,
-      0x00000002.toCSize, // BCRYPT_USE_SYSTEM_PREFERRED_RNG
+      0x00000002.toUInt, // BCRYPT_USE_SYSTEM_PREFERRED_RNG
     )
     if (status > 0x7FFFFFFF.toUInt) {
       throw new RuntimeException(s"BCryptGenRandom returned ${status}")
@@ -97,7 +92,7 @@ private object Bcrypt { // win Bcrypt.h
   def BCryptGenRandom(
     hAlgorithm: CVoidPtr,
     pbBuffer: CVoidPtr,
-    cbBuffer: CUnsignedLong,
-    dwFlags: CUnsignedLong,
+    cbBuffer: UInt,
+    dwFlags: UInt,
   ): UInt = extern
 }
